f7229cf17e76ce68aa1a8c70ff20a9680e49def7
Gregory Vicent <gregory.work14@gmail.com>
2021-10-22 23:22:59 -0400

    added cross-origin permisions

--- src/app.js
+++ src/app.js
@@ -1,6 +1,7 @@
 const express = require("express")
 const morgan = require("morgan")
 const bodyParser = require("body-parser")
+const crossOrigin = require("cross-origin")
 
 const route = require("./router/router.js")
 
@@ -19,6 +20,7 @@ app
 app
   .use(bodyParser.json())
   .use(morgan("dev"))
+  .use(crossOrigin)
   .use(route)
 
 module.exports = app
